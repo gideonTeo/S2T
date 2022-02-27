7177702959d0460b9f89bdd1cd39ec43fe39cb9e
Zhi Kai Teo (Gideon) <zteo0004@student.monash.edu>
2022-02-27 12:42:09 +1100

    Update S2T.py

--- S2T.py
+++ S2T.py
@@ -272,8 +272,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
             if len(sentence) > 5:
                 sentence = sentence[-5:]
 
-        cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
-        cv2.putText(image, ' '.join(sentence), (3, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
 
         # Show to screen
         cv2.imshow('OpenCV Feed', image)
